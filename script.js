9a22e70b3355c184bb25bb9a2f99e343b1e51131
scepedanzale <celesteartuso@hotmail.com>
2025-02-06 16:12:50 +0100

    refactoring codice

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-
+// Lista dei mostri
 const listaMostri = [
     {nome: "Cicciolo", vita: {min: 10, max: 20}, attacco: {min: 5, max: 20}, difesa: {min: 3, max: 10}, perc_fuga: 95},
     {nome: "Freddy", vita: {min: 15, max: 35}, attacco: {min: 15, max: 20}, difesa: {min: 15, max: 30}, perc_fuga: 80},
@@ -9,16 +9,20 @@ const listaMostri = [
     {nome: "Manuel Er Furioso", vita: {min: 100, max: 180}, attacco: {min: 60, max: 90}, difesa: {min: 30, max: 39}, perc_fuga: 5},
 ];
 
-let personaggio = {};
-let mostro = {};
+let personaggio = {}; // dati personaggio
+let mostro = {}; // dati mostro
+
+
 
 // Creazione del personaggio
 
 function creazionePersonaggio(){
     const max = 100;
-    const min = 40;
+    const min = 5;
 
     const nome = prompt("Come si chiama il tuo personaggio?");
+
+    // se il nome è null ferma il programma
     if(nome!=null){
         let vita = 100;
         let attacco = Math.floor(Math.random() * (max - min + 1) + min);
@@ -38,11 +42,13 @@ function creazionePersonaggio(){
 }
 
 
-// Generazione mostrocielo
+// Generazione mostro
 
 function generazioneMostro(){
+    // indice posizione mostro nella lista
     let index = Math.floor(Math.random() * listaMostri.length);
 
+    // dati mostro
     let nome = listaMostri[index].nome
     let vita = Math.floor(Math.random() * (listaMostri[index].vita.max - listaMostri[index].vita.min + 1) + listaMostri[index].vita.min);
     let attacco = Math.floor(Math.random() * (listaMostri[index].attacco.max - listaMostri[index].attacco.min + 1) + listaMostri[index].attacco.min);
@@ -63,49 +69,65 @@ function generazioneMostro(){
 
 function gioco(){
     while(true){
-        let mossa = Number(prompt("Cosa vuoi fare?\n 1. attacco\n 2. cura\n 3. fuggi"))
-    
-        if(isNaN(mossa)){   // Controlla se è un numero
-            console.error("Valore inserito non valido")
-            break;
-        }else{
-            if(mossa===1){  // Attacco
-                mostro.vita = mostro.vita-(personaggio.attacco-mostro.difesa);
-                console.log(`Hai ATTACCATO! La vita del nemico si è abbassata a ${mostro.vita}.`);
-                if (fineGioco()) break;
-            }else if(mossa===2){    // Cura
-                personaggio.vita += Math.floor(Math.random() * (Number(personaggio.vita) - 1) + 1);
-                console.log(`Hai usato una POZIONE! La tua vita si è alzata a ${personaggio.vita}.`)
-            }else if(mossa===3){    // Fuga
-                let num = Math.floor(Math.random() * 100);
-                if(num<=mostro.perc_fuga){
-                    console.log("Sei fuggito! Scampato pericolo...");
-                    break;
-                }else{
-                    console.log("Non puoi scappare!");
-                }
-            }else{
-                break;
-            }
-        }
-
-        attaccoNemico();
-        if (fineGioco()) break;
+        if (attaccoPersonaggio()) break;
+        if (attaccoNemico()) break;
     }
+}
 
 
+// Funzione attacco del personaggio
+
+function attaccoPersonaggio(){
+    // scelta utente
+    let mossa = Number(prompt("Cosa vuoi fare?\n 1. attacco\n 2. cura\n 3. fuggi"))
+    
+    if(isNaN(mossa)){   // Controlla se è un numero
+        console.error("Valore inserito non valido")
+        return true;
+    }else{
+        if(mossa===1){  // Attacco
+            let punti = 0;
+            if(personaggio.attacco<mostro.difesa){  // toglie quantità di punti molto bassa generata casualmente se la sua difesa è più alta del mio attacco
+                punti = Math.ceil(Math.random() * 10);
+            }else{
+                punti = personaggio.attacco-mostro.difesa;
+            }
+            mostro.vita -= punti;
+            if (fineGioco()) return true;;
+            console.log(`Hai ATTACCATO! La vita del nemico si è abbassata a ${mostro.vita}.`);
+        }else if(mossa===2){    // Cura
+            if(personaggio.n_cure===0){ // controlla se ho ancora pozioni
+                console.log(`Non hai più pozioni da utilizzare`);
+            }else{
+                personaggio.vita += Math.floor(Math.random() * (Number(personaggio.vita) - 1) + 1);
+                personaggio.n_cure--;
+                console.log(`Hai usato una POZIONE! La tua vita si è alzata a ${personaggio.vita}.`)
+            }
+        }else if(mossa===3){    // Fuga
+            let num = Math.floor(Math.random() * 100);
+            if(num<=mostro.perc_fuga){  // controllo percentuale di fuga del mostro
+                console.log("Sei fuggito! Scampato pericolo...");
+                return true;
+            }else{
+                console.log("Non puoi scappare!");
+            }
+        }else{
+            return true;
+        }
+    }
 }
 
 // Funzione attacco del mostro
 
 function attaccoNemico(){
     let punti;
-    if(mostro.attacco<personaggio.difesa){  // toglie solo un punto se la mia difesa è più alta dell'attacco del mostro
-        punti = 1;
+    if(mostro.attacco<personaggio.difesa){  // toglie quantità di punti molto bassa generata casualmente se la mia difesa è più alta dell'attacco del mostro
+        punti = Math.ceil(Math.random() * 10);
     }else{
         punti = mostro.attacco-personaggio.difesa
     }
     personaggio.vita -= punti
+    if(fineGioco()) return true;
     console.log(`${mostro.nome} ti ATTACCA! La tua vita si abbassa a ${personaggio.vita}`)
 }
 
@@ -114,10 +136,10 @@ function attaccoNemico(){
 // Gestione fine gioco
 
 function fineGioco(){
-    if(mostro.vita===0){
+    if(mostro.vita<=0){ // vittoria
         console.log(`Hai sconfitto ${mostro.nome}, complimenti!`);
         return true;
-    }else if(personaggio.vita===0){
+    }else if(personaggio.vita<=0){ // sconfitta
         console.log(`Sei stato ucciso da ${mostro.nome}. Sfigato.`);
         return true;
     }
