a1390b182bbfbb218201421b1f78f799e27650b4
scepedanzale <celesteartuso@hotmail.com>
2025-02-06 14:43:53 +0100

    generazione casuale del mostro

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 
-let personaggio = {};
+
 const listaMostri = [
     {nome: "Cicciolo", vita: {min: 10, max: 20}, attacco: {min: 5, max: 20}, difesa: {min: 3, max: 10}},
     {nome: "Freddy", vita: {min: 15, max: 35}, attacco: {min: 15, max: 20}, difesa: {min: 15, max: 30}},
     {nome: "NoSenSe", vita: {min: 20, max: 20}, attacco: {min: 10, max: 20}, difesa: {min: 40, max: 60}},
     {nome: "Moscow Mule", vita: {min: 50, max: 70}, attacco: {min: 30, max: 40}, difesa: {min: 20, max: 40}},
     {nome: "SasageyoSasageyo", vita: {min: 100, max: 150}, attacco: {min: 42, max: 52}, difesa: {min: 40, max: 50}},
-    {nome: "Manuel Er Furioso", vita: {min: 200, max: 300}, attacco: {min: 50, max: 90}, difesa: {min: 45, max: 60}},
+    {nome: "Manuel Er Furioso", vita: {min: 200, max: 300}, attacco: {min: 60, max: 90}, difesa: {min: 45, max: 60}},
 ];
+
+let personaggio = {};
+let mostro = {};
 
 // Creazione del personaggio
 
@@ -22,12 +25,30 @@ function creazionePersonaggio(){
 
     personaggio = {nome, vita, attacco, difesa};
 
-    console.log(personaggio)
+    console.log(`Il tuo personaggio si chiama: ${personaggio.nome}`)
+    console.log(`Vita: ${personaggio.vita}`)
+    console.log(`Attacco: ${personaggio.attacco}`)
+    console.log(`Difesa: ${personaggio.difesa}`)
 }
 
 
-// Generazione mostro
+// Generazione mostrocielo
 
+function generazioneMostro(){
+    let index = Math.floor(Math.random() * listaMostri.length);
+
+    let nome = listaMostri[index].nome
+    let vita = Math.floor(Math.random() * (listaMostri[index].vita.max - listaMostri[index].vita.min + 1) + listaMostri[index].vita.min);
+    let attacco = Math.floor(Math.random() * (listaMostri[index].attacco.max - listaMostri[index].attacco.min + 1) + listaMostri[index].attacco.min);
+    let difesa = Math.floor(Math.random() * (listaMostri[index].difesa.max - listaMostri[index].difesa.min + 1) + listaMostri[index].difesa.min);
+
+    mostro = {nome, vita, attacco, difesa}
+    
+    console.warn(`ATTENTO! Hai incontrato un ${mostro.nome}!`)
+    console.log(`Vita: ${mostro.vita}`)
+    console.log(`Attacco: ${mostro.attacco}`)
+    console.log(`Difesa: ${mostro.difesa}`)
+}
 
 
 // Gestione turni
@@ -42,3 +63,4 @@ function creazionePersonaggio(){
 // Avvio
 
 creazionePersonaggio()
+generazioneMostro()
